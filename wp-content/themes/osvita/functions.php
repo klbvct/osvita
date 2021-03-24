5fa59af87ecf5e9ce7d2b180dd09ed3d5e6da14f
Viktor Kalabukhov <kalabukhov87@gmail.com>
2021-03-24 12:23:38 +0200

    single post

--- wp-content/themes/osvita/functions.php
+++ wp-content/themes/osvita/functions.php
@@ -178,3 +178,7 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/**
+ * Получает миниатюру поста, которая установлена на странице редактирования записи.
+ */
+add_theme_support( 'post-thumbnails' );
