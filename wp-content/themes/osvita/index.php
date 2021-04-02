2dc5a7216ae7c781d32745dfe360ed6b1c66a719
Viktor Kalabukhov <kalabukhov87@gmail.com>
2021-04-02 12:09:16 +0300

    mobilev adaptive

--- wp-content/themes/osvita/index.php
+++ wp-content/themes/osvita/index.php
@@ -10,43 +10,6 @@
 		<h1 class="visually-hidden">Статьи нашего блога</h1>
 		<div class="container content__container">
 			<div class="posts">
-								
-				<?php
-					$result = wp_get_recent_posts( [
-						'numberposts'      => 1,
-						'offset'           => 0,
-						'category'         => 0,
-						'orderby'          => 'post_date',
-						'post_type'        => 'post',
-						'suppress_filters' => true,
-					], OBJECT );
-					foreach( $result as $post ){
-						setup_postdata( $post );?>
-
-						<article class="blog-post blog-post--main">
-							<?php
-								$category = get_the_category();													
-								$cat_link = get_category_link( $category[0] );
-							?>
-							<a href="<?php echo $cat_link; ?>" class="blog-post__category">
-								<?php echo $category[0]->cat_name; ?> 
-							</a>
-							<h3 class="blog-post__title blog-title">
-								<a href="<?php echo get_the_permalink(); ?>" class="blog-post__link">
-									<?php the_title(); ?>
-								</a>
-							</h3>
-							<p class="blog-post__descr">
-								<?php echo get_the_excerpt(); ?>
-							</p>
-							<time class="blog-post__date"><?php the_date(' j F Y '); ?></time>
-						</article>
-
-					<?php 
-					}
-					wp_reset_postdata();
-				?>	
-
 				<ul class="post-grid list-reset">
 				
 				<?php if ( have_posts() ) { while ( have_posts() ) { the_post(); ?>
